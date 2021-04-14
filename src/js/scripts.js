1ba8132d48910208f15cd9c47c53e92a9cc8177d
André Carmo <andre_de11@msn.com>
2021-04-14 13:52:04 -0300

    Implementação da função Javascript que gerencia a seleção única de destińatário e privacidade. Implementação da função que atualiza as informações da bottomBar de acordo com a seleção feita.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,10 +1,38 @@
-
+let selectedRecipient = document.querySelector(".recipientOptions .first");
+let selectedPrivacy = document.querySelector(".privacyOptions .first");
 
 function toggleSidebar() {
     let sidebar = document.querySelector(".sidebar");
     sidebar.classList.toggle("hiddingClass");
 }
 
-function select(element) {
-    
+function selectOption(element) {
+    toggleSelection(element.children[1]);
+
+    if(isRecipientOption(element)) {
+        toggleSelection(selectedRecipient.children[1]);
+        selectedRecipient = element;
+    } else {
+        toggleSelection(selectedPrivacy.children[1]);
+        selectedPrivacy = element;
+    }
+    updateChoices()
+}
+
+function toggleSelection(element) {
+    element.classList.toggle("hiddingClass");
+}
+
+function isRecipientOption(element){
+    return element.parentNode.classList.contains("recipientOptions");
+}
+
+function updateChoices() {
+    let newRecipient = selectedRecipient.children[0].children[1].innerText;
+    let chosenPrivacy = selectedPrivacy.children[0].children[1].innerText;
+    let newInfo = `Enviando para ${newRecipient}`;
+    if (chosenPrivacy === 'Reservadamente') {
+        newInfo +=  ' (reservadamente)';
+    }
+    document.querySelector(".bottomBar p").innerHTML = newInfo;
 }
